35400cfd75fc1b57d1194fbab0a9f1d694d13a40
Adam89 <Adamm1@aston.ac.uk>
2013-09-17 23:04:27 +0100

    finished using .each on all of these test methods

--- are_there_any_test.rb
+++ are_there_any_test.rb
@@ -1,43 +1,56 @@
 gem 'minitest'
-require 'minitest'
+require 'minitest/autorun'
 require 'minitest/pride'
 
 class AreThereAnyTest < Minitest::Test
   def test_has_a_zero
     numbers = [2, 9, 3, 1, 0]
-    has_zero = false
+    has_zero = [0]
+    remaining = []
     numbers.each do |number|
-      # write code here
+      remaining << number if number == 0
     end
-    assert has_zero
+    assert_equal has_zero, remaining
   end
 
   def test_has_zeros
-    skip
     numbers = [3, 1, 0, 7, 9, 0]
-    # write code here
-    assert has_zero
+    has_zero = [0, 0]
+    remaining = []
+    numbers.each do |num|
+      remaining << num if num == 0
+    end
+    assert_equal has_zero, remaining
   end
 
   def test_does_not_have_zero
-    skip
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    # write code here
-    refute has_zero
+    has_zero = [0]
+    remaining = []
+    numbers.each do |num|
+      remaining << num if num != 0
+    end
+    refute_equal has_zero, remaining
   end
 
   def test_no_alice
-    skip
     names = %w(chuck charlene cory chris carl)
-    # write code here
-    refute has_alice
+    has_alice = ['Alice', 'alice']
+    remaining = []
+    names.each do |name|
+      remaining << name unless name == 'alice'
+    end
+    refute_equal has_alice,  remaining
   end
 
   def test_has_alice
-    skip
     names = %w(bill bob burton alice brandon)
-    # write code here
-    assert has_alice
+    has_alice = ['alice']
+    remaining =[]
+    names.each do |name|
+      remaining << name if name == 'alice'
+    end
+    assert_equal has_alice, remaining
   end
 end
 
--- are_they_all_test.rb
+++ are_they_all_test.rb
@@ -1,51 +1,67 @@
 gem 'minitest'
-require 'minitest'
+require 'minitest/autorun'
 require 'minitest/pride'
 
 class AreTheyAllTest < Minitest::Test
 
   def test_not_all_zeros
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
-    all_zeros = true
+    all_zeros = false
+    remaining = []
     numbers.each do |number|
-      # write code here
+      remaining << number if number != 0
     end
-    refute all_zeros
+    refute_equal all_zeros, remaining
   end
 
   def test_all_zeros
-    skip
     numbers = [0, 0, 0, 0, 0, 0, 0, 0]
-    # write code here
-    assert all_zeros
+    all_zeros = [0, 0, 0, 0, 0, 0, 0, 0]
+    remaining = []
+    numbers.each do |num|
+      remaining << num if num == 0
+    end
+    assert_equal all_zeros, remaining
   end
 
   def test_all_gone
-    skip
     words = %w(gone gone gone gone gone gone gone)
-    # write code here
-    assert all_gone
+    all_gone = %w(gone gone gone gone gone gone gone)
+    remaining = []
+    words.each do |word|
+      remaining << word if word == 'gone'
+    end
+    assert_equal all_gone, remaining
   end
 
   def test_not_all_gone
-    skip
     words = %w(gone gone gone gone gone gone gone yepp)
-    # write code here
-    refute all_gone
+    all_gone = %w(gone gone gone gone gone gone gone yepp)
+    remaining = []
+    words.each do |word|
+      remaining << word if word != 'gone'
+    end
+    refute_equal all_gone, remaining
   end
 
   def test_all_empty
-    skip
     words = ["", "", "", "", ""]
-    # write code here
-    assert all_empty
+    all_empty = ["", "", "", ""]
+    remaining = []
+    words.each do |word|
+      remaining << word if word == ""
+    end
+    assert all_empty, remaining
   end
 
   def test_not_all_empty
-    skip
     words = ["full", "", "", "", "", ""]
-    # write code here
-    refute all_empty
+    all_empty =["full", "", "", "", "", ""]
+    remaining = []
+    words.each do |word|
+      remaining << word if word != ""
+    end
+    refute_equal all_empty, remaining
   end
 end
 
